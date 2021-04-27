0bf809582175570820827b5502332fa1f39c7b2b
kokosnica <78181419+kokosnica@users.noreply.github.com>
2021-04-28 00:04:23 +0200

    Update tuinity.sh

--- eggs/tuinity.sh
+++ eggs/tuinity.sh
@@ -1,7 +1,7 @@
 cd /mnt/server
 
 if [ ! -f server.properties ]; then
-curl -o server.properties https://raw.githubusercontent.com/kokosnica/tuinity-optimized/main/server.properties
+curl -o server.properties https://raw.githubusercontent.com/kokosnica/kh-egg/main/server.properties
 fi
 
 curl -o ${SERVER_JARFILE} https://ci.codemc.io/job/Spottedleaf/job/Tuinity/lastStableBuild/artifact/tuinity-paperclip.jar
