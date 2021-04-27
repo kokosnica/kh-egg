2c651e85206a025cd23c4186cbd822e260296fb8
kokosnica <78181419+kokosnica@users.noreply.github.com>
2021-04-28 00:13:54 +0200

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,57 @@
+# check for curl
+if ! [ -x "$(command -v curl)" ]; then
+  echo "* curl is required in order for this script to work."
+  echo "* install using apt (Debian and derivatives) or yum/dnf (CentOS)"
+  exit 1
+fi
+
+output() {
+  echo "* ${1}"
+}
+
+error() {
+  COLOR_RED='\033[0;31m'
+  COLOR_NC='\033[0m'
+
+  echo ""
+  echo -e "* ${COLOR_RED}ERROR${COLOR_NC}: $1"
+  echo ""
+}
+
+vanilla=false
+papermc=false
+forge=false
+tuinity=false
+tuinity-opt=false
+yatopia=false
+custom=false
+
+output "K-Hosting egg installer"
+
+output
+
+case $EGG in
+      1 )
+          vanilla=true ;;
+      2 )
+          papermc=true ;;
+      3 )
+          forge=true ;;
+      4 )
+          tuinity=true ;;
+      5 )
+          tuinity-opt=true ;;
+      6 )
+          yatopia=true ;;
+      7 )
+          custom=true ;;
+      * )
+          error "Invalid option" ;;
+
+[ "$vanilla" == true ] && bash <(curl -s https://raw.githubusercontent.com/kokosnica/kh-egg/main/eggs/vanilla.sh)
+[ "$papermc" == true ] && bash <(curl -s https://raw.githubusercontent.com/kokosnica/kh-egg/main/eggs/papermc.sh)
+[ "$forge" == true ] && bash <(curl -s https://raw.githubusercontent.com/kokosnica/kh-egg/main/eggs/forge.sh)
+[ "$tuinity" == true ] && bash <(curl -s https://raw.githubusercontent.com/kokosnica/kh-egg/main/eggs/tuinity.sh)
+[ "$tuinity-opt" == true ] && bash <(curl -s https://raw.githubusercontent.com/kokosnica/kh-egg/main/eggs/tuinity-opt.sh)
+[ "$yatopia" == true ] && bash <(curl -s https://raw.githubusercontent.com/kokosnica/kh-egg/main/eggs/yatopia.sh)
+[ "$custom" == true ] && bash <(curl -s https://raw.githubusercontent.com/kokosnica/kh-egg/main/eggs/custom.sh)
